9f16afaa6636ad2bb28e5f647b3e7a7e13c2fe42
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-11 15:05:35 +0300

    Initial Commit

---
+++ local api test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  local api test
+//
+//  Created by Abdirizak Hassan on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
