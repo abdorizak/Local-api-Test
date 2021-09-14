b243152b5747e4c503bebb1d88677005cf4e92a6
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-14 12:53:25 +0300

    first commit

---
+++ local api test/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  local api test
+//
+//  Created by Abdirizak Hassan on 9/11/21.
+//
+
+import Foundation
---
+++ local api test/Users.swift
@@ -0,0 +1,8 @@
+//
+//  Users.swift
+//  local api test
+//
+//  Created by Abdirizak Hassan on 9/11/21.
+//
+
+import Foundation
